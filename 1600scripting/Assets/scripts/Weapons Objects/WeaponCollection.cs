ea3684b72b706972ebda2bbcb804af3935590f2d
Unknown <wrevlis@yahoo.com>
2018-06-27 10:11:30 -0600

    more Weapons

---
+++ 1600scripting/Assets/scripts/Weapons Objects/WeaponCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WeaponCollection : ScriptableObject
+{
+
+    public List<WeaponObject> Weapons;
+    public void CollectWeapon (WeaponObject weapon)
+    {
+        Weapons.Add(weapon);
+
+        foreach (var item in Weapons)
+        {
+            Debug.Log(item);
+            if (item.name == "sword")
+            {
+                Debug.Log("You Have A Sword");
+            }
+        }
+    }
+
+}
---
+++ 1600scripting/Assets/scripts/Weapons Objects/WeaponPickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickUp : MonoBehaviour
+{
+
+    public WeaponObject Weapon;
+    public WeaponCollection Collection;
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        Collection.CollectWeapon(Weapon);
+    }
+
+}
