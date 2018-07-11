be7d279d7b742c858c1832dbd4d231b4e9802f3d
Unknown <wrevlis@yahoo.com>
2018-07-11 10:20:48 -0600

    wepons and fab

---
+++ 1600scripting/Assets/scripts/WhileLoops.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhileLoops : MonoBehaviour
+{
+
+    public Text DisplayText;
+    public int i = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Ticker());
+    }
+        IEnumerator Ticker()
+        {
+            while (i > 0)
+            {
+                DisplayText.text = i.ToString();
+                yield return new WaitForSeconds(1);
+                i--;
+            }
+            DisplayText.text = "GO";
+            yield return new WaitForSeconds(2);
+            DisplayText.text = "";
+        }
+}
