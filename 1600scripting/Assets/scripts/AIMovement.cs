c935d9b55cf1b37a21b8f216df8c09679a70ac70
Unknown <wrevlis@yahoo.com>
2018-08-08 11:03:55 -0600

    it's all Together

--- 1600scripting/Assets/scripts/AIMovement.cs
+++ 1600scripting/Assets/scripts/AIMovement.cs
@@ -11,6 +11,14 @@ public class AIMovement : MonoBehaviour
     public Transform Destination;
     public AIControll Control;
 
-
+    private void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        Control.ConfigAI(Agent);
+    }
 	
+    private void Update()
+    {
+        Agent.destination = Destination.position;
+    }
 }
